f79fcc5d01a4e552b81077fdefa1b57652f29bd1
jaime <jaime>
2026-01-07 16:00:59 +0100

    Initial Commit

---
+++ Gym/GymApp.swift
@@ -0,0 +1,32 @@
+//
+//  GymApp.swift
+//  Gym
+//
+//  Created by Jaime Arriba on 07/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GymApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
